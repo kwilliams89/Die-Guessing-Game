f7ea4db73d57bc4edd619edacedce38f6394f0f8
Kevin <kevinwilliams4@mail.weber.edu>
2017-09-18 11:18:37 -0600

    Add project files.

---
+++ KWilliams_CS3280_Assignment2/DieGuess.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace KWilliams_CS3280_Assignment2
+{
+    partial class DieGuess
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtGuess = new System.Windows.Forms.TextBox();
+            this.btnRoll = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.pbImage = new System.Windows.Forms.PictureBox();
+            this.lblMessage = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(30, 22);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(253, 122);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Game Info";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(173, 84);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 13);
+            this.label6.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(173, 56);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(173, 27);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(15, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Number of Times Lost:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(116, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Number of Times Won:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(125, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Number of Times Played:";
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(30, 286);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(450, 145);
+            this.listView1.TabIndex = 2;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 179);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(113, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Enter your guess (1-6):";
+            // 
+            // txtGuess
+            // 
+            this.txtGuess.Location = new System.Drawing.Point(146, 176);
+            this.txtGuess.MaxLength = 1;
+            this.txtGuess.Name = "txtGuess";
+            this.txtGuess.Size = new System.Drawing.Size(32, 20);
+            this.txtGuess.TabIndex = 7;
+            // 
+            // btnRoll
+            // 
+            this.btnRoll.Location = new System.Drawing.Point(208, 214);
+            this.btnRoll.Name = "btnRoll";
+            this.btnRoll.Size = new System.Drawing.Size(75, 23);
+            this.btnRoll.TabIndex = 8;
+            this.btnRoll.Text = "Roll";
+            this.btnRoll.UseVisualStyleBackColor = true;
+            this.btnRoll.Click += new System.EventHandler(this.btnRoll_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(208, 243);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 9;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // pbImage
+            // 
+            this.pbImage.Location = new System.Drawing.Point(73, 216);
+            this.pbImage.Name = "pbImage";
+            this.pbImage.Size = new System.Drawing.Size(67, 50);
+            this.pbImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbImage.TabIndex = 10;
+            this.pbImage.TabStop = false;
+            // 
+            // lblMessage
+            // 
+            this.lblMessage.AutoSize = true;
+            this.lblMessage.Location = new System.Drawing.Point(225, 183);
+            this.lblMessage.Name = "lblMessage";
+            this.lblMessage.Size = new System.Drawing.Size(0, 13);
+            this.lblMessage.TabIndex = 11;
+            // 
+            // DieGuess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(499, 443);
+            this.Controls.Add(this.lblMessage);
+            this.Controls.Add(this.pbImage);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnRoll);
+            this.Controls.Add(this.txtGuess);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "DieGuess";
+            this.Text = "Die Guess Game";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtGuess;
+        private System.Windows.Forms.Button btnRoll;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.PictureBox pbImage;
+        private System.Windows.Forms.Label lblMessage;
+    }
+}
+
---
+++ KWilliams_CS3280_Assignment2/DieGuess.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace KWilliams_CS3280_Assignment2
+{
+    public partial class DieGuess : Form
+    {
+
+        #region Variables
+
+        private Random rnd;   // random Object for random number generator
+        private int guess;      // store user guess
+        private int dieRolled;  // store simulated die value
+        private int tPlayed;    // store times played
+        private int tWon;       // store times won
+        private int tLost;      // store times lost
+        private int[] guessCounter;     //array to hold guess counter for each value 1-6
+        private int[] rollCounter;      //array to hold roll counter for each value 1-6
+        string[] arr;  // declare and initialize arr for adding items to listview
+        ListViewItem item;
+
+
+        #endregion
+
+        #region Guess Methods
+        /// <summary>
+        /// Constructor initalizes form and sets default values.
+        /// </summary>
+        public DieGuess()
+        {
+            InitializeComponent();
+            rnd = new Random();  // initialize random object
+            guessCounter = new int[6] { 0, 0, 0, 0, 0, 0 };
+            rollCounter = new int[6] { 0, 0, 0, 0, 0, 0 };
+            arr = new string[4];
+
+            //initialize statistics and displays for statistics to initial values
+            tPlayed = 0;
+            tWon = 0;
+            tLost = 0;
+            label4.Text = tPlayed.ToString();
+            label5.Text = tWon.ToString();
+            label6.Text = tLost.ToString();
+
+            listView1.View = View.Details;
+            listView1.FullRowSelect = true;
+
+            listView1.Columns.Add("FACE", 50);
+            listView1.Columns.Add("FREQUENCY", 100);
+            listView1.Columns.Add("PERCENT", 100);            
+            listView1.Columns.Add("NUMBER OF TIMES GUESSED", 195);
+
+            for (int i = 0; i < 6; i++)             //for loop to add/update items in listview       
+            {
+                arr[0] = (i+1).ToString();
+                arr[1] = 0.ToString();
+                arr[2] = 0.ToString();
+                arr[3] = 0.ToString();
+                item = new ListViewItem(arr);
+                item.Name = (i + 1).ToString();
+                listView1.Items.Add(item);
+
+            }
+
+        }
+        
+        /// <summary>
+        /// Method simulates die roll.
+        /// </summary>
+        private void simulateRoll()
+        {
+            // simulate die roll
+            for (int i = 1; i < 7; i++) 
+            {
+                dieRolled = rnd.Next(1, 7); // generate random die number
+                pbImage.Image = Image.FromFile("die" + dieRolled.ToString() + ".gif");  //display die image
+                pbImage.Refresh();
+                Thread.Sleep(300);
+            }
+            rollCounter[dieRolled - 1]++;
+
+        }
+        /// <summary>
+        /// Retrieves user guess from textbox. Idea adapted from answer on stackoverflow from T.Rob. Link to answer - http://stackoverflow.com/a/8199613
+        /// </summary>
+        private void retrieveGuess()
+        {
+
+            int number;
+            bool isNumber;
+            isNumber = Int32.TryParse(txtGuess.Text, out number);
+
+            if (!isNumber)
+            {
+                guess = 0; // set guess to out of bounds value to force new input when input was invalid
+            }
+            else
+            {
+                guess = Convert.ToInt32(txtGuess.Text); // converts textbox value to int and sets guess equal to the value
+            }
+
+        }
+        /// <summary>
+        /// Compares user guess to simulate die value. Updates game counters accordingly.
+        /// </summary>
+        /// <param name="roll"></param>
+        /// <param name="guess"></param>
+        private void compareGuess(int roll, int guess)
+        {
+            tPlayed++;  // increment tPlayed for valid guess
+            guessCounter[guess - 1]++;
+            if (guess == dieRolled)  // compare user guess to die value.
+            {
+                tWon++;  // increment tWon for correct guess
+                lblMessage.Text = "You guessed correctly!";
+            }
+            else
+            {
+                tLost++;  // increment tLost for incorrect guess
+                lblMessage.Text = "You guessed wrong!";
+            }
+        }
+        /// <summary>
+        /// Method updates game statistics.
+        /// </summary>
+        private void updateStats()
+        {
+            label4.Text = tPlayed.ToString();  // Display updated times played.
+            label5.Text = tWon.ToString();     // Display updated times won.
+            label6.Text = tLost.ToString();     //Display updated times lost.
+
+
+            for (int i = 0; i < 6; i++)             //for loop to add/update items in listview       
+            {
+                listView1.Items[i].SubItems[1].Text = rollCounter[i].ToString();   //update frequency column
+                double percentage = (double)rollCounter[i] / (double)tPlayed * 100;               //calculate frequency percentage
+                percentage = Math.Round(percentage, 2); //round to two decimal places
+                listView1.Items[i].SubItems[2].Text = percentage.ToString() + "%";        //update percentage column
+                listView1.Items[i].SubItems[3].Text = guessCounter[i].ToString();   //update guess column
+            }
+            txtGuess.Text = "";
+        }
+
+        #endregion
+
+        #region Form Methods
+        /// <summary>
+        /// Method initiates individual game round via roll button.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnRoll_Click(object sender, EventArgs e)
+        {
+            retrieveGuess();
+
+            if (guess > 0 && guess < 7)
+            {
+                simulateRoll();
+                compareGuess(dieRolled, guess);
+                updateStats();
+            }
+            else
+            {
+                lblMessage.Text = "Invalid Input. Please enter a value between 1-6.";
+            }
+
+        }
+
+        /// <summary>
+        /// Method resets game board. 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            Application.Restart(); // refresh game board
+        }
+
+        #endregion
+    }
+}
